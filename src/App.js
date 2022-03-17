3b0157ec2387716cd19ff22b389d2c9f5a0f4703
fluturecode <fluturecode@gmail.com>
2022-03-16 20:58:35 -0400

    added chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import { Box, Center, Heading, Stack } from "@chakra-ui/react";
-import { ByMoralis } from "react-moralis";
-import { WatchAddress } from "./Forms/WatchAddress";
+import { Box, Center, Heading, Stack } from '@chakra-ui/react';
+import { ByMoralis } from 'react-moralis';
+import { WatchAddress } from './Forms/WatchAddress';
 
 export const App = () => {
-
   return (
     <Center>
-    <Box w={450}>
-      <Center>
-      <Heading m={4} as="h1" size="2xl" isTruncated>🐋🚨</Heading>
-      </Center>
-      <Stack spacing={3}>
-        <Box>
-        {
-          // form begins here
-        }
-          <WatchAddress />
+      <Box w={450}>
+        <Center>
+          <Heading m={4} as='h1' size='2xl' isTruncated>
+            🐋🚨
+          </Heading>
+        </Center>
+        <Stack spacing={3}>
+          <Box>
+            {
+              // form begins here
+            }
+            <WatchAddress />
+          </Box>
+        </Stack>
+        <Box float='right' mt={4}>
+          <ByMoralis width={200} variant='dark' />
         </Box>
-      </Stack>
-      <Box float="right" mt={4}>
-        <ByMoralis width={200} variant="dark"/>
       </Box>
-    </Box>
     </Center>
   );
-}
+};
